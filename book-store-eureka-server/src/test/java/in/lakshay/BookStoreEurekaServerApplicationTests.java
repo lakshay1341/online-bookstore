1ad554350641760343059a97305367bb8c451208
Lakshay <63228418+lakshay1341@users.noreply.github.com>
2025-03-06 01:35:34 +0530

    initial eureka commit

---
+++ book-store-eureka-server/src/test/java/in/lakshay/BookStoreEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.lakshay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
