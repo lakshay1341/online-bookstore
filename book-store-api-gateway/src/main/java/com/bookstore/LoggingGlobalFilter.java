0bb8e62f055084cf44c534a04aacd31a3f455ea5
Lakshay <63228418+lakshay1341@users.noreply.github.com>
2025-03-06 01:34:47 +0530

    initial api gateway commit

---
+++ book-store-api-gateway/src/main/java/com/bookstore/LoggingGlobalFilter.java
@@ -0,0 +1,21 @@
+package com.bookstore;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingGlobalFilter implements GlobalFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingGlobalFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Incoming request: {} {}", exchange.getRequest().getMethod(), exchange.getRequest().getURI());
+        return chain.filter(exchange);
+    }
+}
